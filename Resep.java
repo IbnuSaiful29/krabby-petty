5da33d45fa0e79e1c2b81e95cb8170c2fa393849
Ibnu Saiful <ibnusaiful1933@gmail.com>
2022-10-06 10:59:15 +0700

    Tambahan resep Krabby Petty

--- Resep.java
+++ Resep.java
@@ -7,7 +7,13 @@ public class Resep {
         System.out.println("Resep Krabby Petty");
         System.out.println("------------------");
         System.out.println("1. Daging");
-        System.out.println("2. Roti");
+        System.out.println("2. Roti Bun");
+        System.out.println("3. Sous");
+        System.out.println("4. Selada");
+        System.out.println("5. Acar");
+        System.out.println("6. Tomat");
+        System.out.println("7. Keju");
+
 
     }
 }
