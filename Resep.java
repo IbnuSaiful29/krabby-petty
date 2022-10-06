ae9886dcee188cb283207e7beee9a8fd2e0a471a
Ibnu Saiful <ibnusaiful1933@gmail.com>
2022-10-06 10:42:15 +0700

    inisiasi resep

---
+++ Resep.java
@@ -0,0 +1,13 @@
+/**
+ * Resep
+ */
+public class Resep {
+
+    public static void main(String[] args) {
+        System.out.println("Resep Krabby Petty");
+        System.out.println("------------------");
+        System.out.println("1. Daging");
+        System.out.println("2. Roti");
+
+    }
+}
